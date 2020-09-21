10bd947e16f8ca1d3bc5ef02b4dc09f7192b0303
Jhon Deiber Arcila Castaño <Jhon.380172793@ucaldas.edu.co>
2020-09-21 13:06:50 -0500

    pagina de registro de usuarios PHP

---
+++ views/contenido.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar Sesion</a>
+       <div class="contenido">
+       <article>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam sapiente maxime veritatis architecto vero veniam quidem eius aspernatur doloribus necessitatibus. Nobis fuga maxime assumenda ipsam aut porro fugiat consequatur aspernatur.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi modi nobis rerum quidem vel atque consequuntur sequi quisquam, dolorum est officia quam, architecto mollitia ratione cum culpa in placeat aspernatur!</p>
+            
+        </article>
+       </div>
+    </div>
+</body>
+</html>
